db7ab6031beb359af9dd11e0c6ce2397804a2421
Nir Gutman <46134640+NGBigField@users.noreply.github.com>
2020-05-15 18:58:13 +0300

    class_defs

---
+++ class_defs.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+## import depanding on running state / configuration state:
+from config import CONFIG, ConfigEnum, IS_DEBUG_MODE 
+
+if (CONFIG == ConfigEnum.REAL_TIME) or (CONFIG == ConfigEnum.COGNATA_SIMULATION):
+    from pyFormulaClient import messages
+    from pyFormulaClient.MessageDeque import NoFormulaMessages
+elif CONFIG == ConfigEnum.LOCAL_TEST:
+    from pyFormulaClientNoNvidia import messages
+    from pyFormulaClientNoNvidia.MessageDeque import NoFormulaMessages
+else:
+    raise NameError("User Should Choose Configuration from config.py")
+
+
+class ConeType(Enum):
+    YELLOW      = messages.perception.Yellow
+    BLUE        = messages.perception.Blue
+    ORANGE_BIG  = messages.perception.OrangeBig
+    ORANGE_SMALL= messages.perception.Orange
