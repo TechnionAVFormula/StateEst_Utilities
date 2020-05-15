0e744eb27adb6c2bfe7a984a7ba8afa691b187f2
Nir Gutman <46134640+NGBigField@users.noreply.github.com>
2020-05-15 21:10:03 +0300

    Minor

--- class_defs.py
+++ class_defs.py
@@ -2,16 +2,25 @@ from enum import Enum
 
 ## import depanding on running state / configuration state:
 from config import CONFIG, ConfigEnum, IS_DEBUG_MODE 
-from Messages import messages , NoFormulaMessages
 
-# if (CONFIG == ConfigEnum.REAL_TIME) or (CONFIG == ConfigEnum.COGNATA_SIMULATION):
-#     from pyFormulaClient import messages
-#     from pyFormulaClient.MessageDeque import NoFormulaMessages
-# elif CONFIG == ConfigEnum.LOCAL_TEST:
-#     from pyFormulaClientNoNvidia import messages
-#     from pyFormulaClientNoNvidia.MessageDeque import NoFormulaMessages
-# else:
-#     raise NameError("User Should Choose Configuration from config.py")
+
+if (CONFIG == ConfigEnum.REAL_TIME) or (CONFIG == ConfigEnum.COGNATA_SIMULATION):
+    from pyFormulaClient import messages
+    from pyFormulaClient.MessageDeque import NoFormulaMessages
+elif CONFIG == ConfigEnum.LOCAL_TEST:
+    from pyFormulaClientNoNvidia import messages
+    from pyFormulaClientNoNvidia.MessageDeque import NoFormulaMessages
+else:
+    raise NameError("User Should Choose Configuration from config.py")
+
+
+
+
+''' class defs and enums that can be imported: '''
+'''============================================'''
+
+IMPORT_NoFormulaMessages = NoFormulaMessages
+IMPORT_messages = messages
 
 
 class ConeType(Enum):
